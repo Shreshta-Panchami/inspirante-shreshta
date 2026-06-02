8512b192cf75abbc20a84fd12cc50a0d4214cbea
Shreshta-Panchami <shreshtapoojary27@gmail.com>
2026-06-02 14:34:59 +0530

    Display events on student dashboard

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const Event = require("../models/Event");
 
+// Create Event
 router.post("/", async (req, res) => {
-
   try {
 
     const { name, date, venue, capacity } = req.body;
@@ -29,6 +29,27 @@ router.post("/", async (req, res) => {
     res.status(500).json({
       message: "Server error"
     });
+
+  }
+});
+
+// Get All Events
+router.get("/", async (req, res) => {
+
+  try {
+
+    const events = await Event.find().sort({ date: 1 });
+
+    res.json(events);
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server error"
+    });
+
   }
 
 });
